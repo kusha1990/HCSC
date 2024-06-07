93fe6064079406645d3e610ab4b62454b05d244c
kusha1990 <cg.kusha@gmail.com>
2024-06-07 11:00:55 +0530

    updated line in app.java file

--- hcsc1/src/main/java/HCSC_project/hcsc1/App.java
+++ hcsc1/src/main/java/HCSC_project/hcsc1/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!, This is HCSC1 project" );
+        System.out.println("update line to git repo");
     }
 }
