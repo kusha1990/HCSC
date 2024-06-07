d926d5c1da6d8eb856811030fa605954050710ac
kusha1990 <kushacgaws@gmail.com>
2024-06-07 10:40:08 +0530

    hcsc project file added

---
+++ hcsc1/src/main/java/HCSC_project/hcsc1/App.java
@@ -0,0 +1,13 @@
+package HCSC_project.hcsc1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!, This is HCSC1 project" );
+    }
+}
